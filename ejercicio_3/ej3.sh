4174f1846bd68d6d00e8fd83eb1cf53d29fae6c0
Dito <ditommaso.giuliano@gmail.com>
2019-10-05 14:49:07 -0300

    ejercicio_3_parcial

--- ejercicio_3/ej3.sh
+++ ejercicio_3/ej3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 accion=$1
-#echo $$
 
 ayuda(){
 	echo 'Usage:'
@@ -13,75 +12,100 @@ ayuda(){
 
 PIDFILE=$(pwd)
 PIDFILE+="/daemonpid"
-touch ej3.log
 
 PATHTOBACKUPDIR="pathToBackUpDir";
 
 LOGFILE=$(pwd)
 LOGFILE+="/ej3.out"
-case $accion in 
-start)
-	if [ $# -lt 4 ]
+
+
+check_parameters_start(){
+	if [ $1 -lt 4 ]
 	then
-    echo "Parametros insuficientes. Utilice -h o -help para conocer el funcionamiento"
-    exit
+     echo "Parametros insuficientes. Utilice -h o -help para conocer el funcionamiento"
+     exit
 	fi
-	if [ ! -f ej3.out ]
-	then
+}
+
+generar_archivo_log(){
+   if [ ! -f ej3.out ]
+   then
 		touch ej3.out
-	fi
-	if [ -f $PIDFILE ]
+   fi
+}
+
+verificar_ejecucion(){
+	CANTPROCESOS=$( ps aux | grep ej3.daemon.sh -c ) 
+	if [ $CANTPROCESOS -gt  2 ] 
 	then
-		CANTPROCESOS=$( ps aux | grep ej3.daemon.sh -c ) 
-		if [ $CANTPROCESOS -gt  1 ] 
-		then
-			echo "No se puede ejecutar.Se esta ejecutando en este momento."
-		exit
+		if [ $1 == 'false' ]
+		then	
+		   echo "No se puede ejecutar.Se esta ejecutando en este momento."
+		   exit
+		fi 
+	else
+		if [ $1 == 'true' ]
+		then	
+			echo "No hay instancia corriendo"
+			exit
 		fi	
-	fi
+	fi	
+}
+
+guardar_path_backup(){
 	if [ ! -f pathToBackUpDir ] 
 	then
 		touch pathToBackUpDir 
 	fi
-	echo $3 >| pathToBackUpDir 
-	intervalo=$4
-	#directorio=$(pwd)
-	#directorio+="/ej3_daemon.sh"
-	#start-stop-daemon --start --startas /bin/bash --pidfile=$PIDFILE -- -c "exec $directorio $2 $3 $4 >> $LOGFILE 2>&1" --make-pidfile --background
-	nohup sh ej3_daemon.sh  $2 $3 $4 > $LOGFILE &
-	exit
-	;;
-stop) 
-	#start-stop-daemon --stop --quiet --pidfile=$PIDFILE
-	
-	if [ ! -f $PIDFILE ]
+	echo $1 >| pathToBackUpDir 	
+}
+
+
+guardar_path_dir_guardar(){
+	if [ ! -f pathDirGuardar ] 
 	then
-		echo "No hay instancia ejecutando"
-		exit
+		touch pathDirGuardar 
 	fi
+	echo $1 >| pathDirGuardar	
+}
+
+case $accion in 
+start)
+	check_parameters_start $#
+ 	generar_archivo_log
+	verificar_ejecucion false
+	guardar_path_dir_guardar "$2"
+	guardar_path_backup "$3"
+	./ej3_daemon.sh "$4" &>> $LOGFILE &
+	exit
+	;; 
+stop) 
+	verificar_ejecucion true
 	pidDaemon=$(<$PIDFILE)
-	kill $pidDaemon
+	kill -9 $pidDaemon
+	rm $PIDFILE
 	exit
 	;;
 count)
-		DIRBACKUP=$(<$PATHTOBACKUPDIR) 
-		echo $(find $DIRBACKUP -type f | wc -l)
-		;;
+	DIRBACKUP=$(<$PATHTOBACKUPDIR) 
+	echo $(find $DIRBACKUP -type f | wc -l)
+	;;
 clear)
-		##if[ $# -gt 0 ]
-		##then
-		#	echo 'cantidad de backup'
-		#else
-		#	rm -rf '$*'
-		#fi;
-		;;
-play)
-	if  $# -lt 3 ; then
-		echo "Parametros insuficientes. Utilice -h o -help para conocer el funcionamiento"
-		exit
+	DIRBACKUP=$(<$PATHTOBACKUPDIR)
+	if [ $# -gt 1 ]
+	then
+		CANTIDADMANTENER="$2"
+		cd $DIRBACKUP
+		ls -t | sed -e '1,'"$CANTIDADMANTENER"'d' | xargs -d '\n' rm 
+	else
+		DIRBACKUP=$(<$PATHTOBACKUPDIR)
+		rm $DIRBACKUP/*.tar.gz
 	fi
-	
-	cp -r $1 $2
+	;;
+play)
+	verificar_ejecucion true
+	pidDaemon=$(<$PIDFILE)
+	kill -SIGTERM $pidDaemon &>> $LOGFILE &
 	;;
 -h|-help|-?) ayuda
 		;;
--- ejercicio_3/ej3_daemon.sh
+++ ejercicio_3/ej3_daemon.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-BACKGROUND=false
-if [! -f daemonpid]
+
+do_backup(){
+	hora=$(date +%Y%m%d%H%M%S)
+	PATHBACKUP=$(<pathToBackUpDir) 
+	PATHAGUARDAR=$(<pathDirGuardar)
+	echo  " $hora : Haciendo backup de $PATHAGUARDAR en $PATHBACKUP"
+	FILENAME=backup_$hora.tar.gz
+	tar -czvf $FILENAME $PATHAGUARDAR
+	mv $FILENAME $PATHBACKUP
+}
+
+
+trap do_backup SIGTERM
+ 
+if [ ! -f daemonpid ]
 then
 	touch daemonpid
 fi
-while true; do
-	if $background ; then	
-		echo $$ >| daemonpid
-	fi
-	if  !$background ; then
-		$background=true
-	fi	
-	hora=$(date +%Y%m%d%H%M%S)
-	echo  " $hora : Haciendo backup de $1 en $2"
-	cp -r $1 $2
-	sleep $3
+echo $$ >| daemonpid
+while true; do	
+	do_backup
+	sleep $1 &
+	wait;
 done
 
 	
